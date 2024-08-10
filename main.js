7316070e3f356534a85fe37114504bba7ad6ca01
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-08-10 15:35:26 +0100

    Updated added

--- main.js
+++ main.js
@@ -7,3 +7,14 @@ showAndHideSideBar.addEventListener('click', (event)=>{
         mobileSideBar.style.visibility = 'hidden'
     }
 })
+
+document.querySelectorAll('button').forEach((btn)=>{
+    btn.addEventListener('click', (event)=>{
+        alert('Sunnyside is not a real brand. Its a demo project I worked on')
+    })
+})
+document.querySelectorAll('li').forEach((list)=>{
+    list.addEventListener('click', (event)=>{
+        alert('Sunnyside is not a real brand. Its a demo project I worked onSunnyside is not a real brand. Its a demo project I worked on')
+    })
+})
